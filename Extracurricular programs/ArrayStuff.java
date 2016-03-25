049b7282ee4e340b7a07a31d8e15b7544e74759b
Connor Baker <ConnorBaker01@Gmail.com>
2016-03-25 12:45:49 -0400

    Introduce several new methods

--- Extracurricular programs/ArrayStuff.java
+++ Extracurricular programs/ArrayStuff.java
@@ -23,14 +23,20 @@ import java.util.Arrays;
 
 public class ArrayStuff {
   public static void main(String[] args) {
-    int[][][] arrayFirst = createArray();
-    printArray(arrayFirst);
-    fillArray(arrayFirst);
-    printArray(arrayFirst);
-    int[][][] arraySecond = createArray();
-    printArray(arraySecond);
-    fillArray(arraySecond);
-    printArray(arraySecond);
+    int[][][] master_array = create3dArray();
+
+    // Fill master_array
+    for (int i = 0; i < master_array.length; i++) {
+      master_array[i] = fillArray();
+      // for (int j = 0; j < master_array[i].length; j++) {
+      //   master_array[i][j] = randomFill();
+      //   for (int k = 0; k < master_array[j].length; k++) {
+      //     master_array[i][j][k] = randomFill();
+      //   }
+      // }
+    }
+
+    print3dArray(master_array);
   }
 
   /*
@@ -39,8 +45,13 @@ public class ArrayStuff {
   *  based on that.
   */
 
-  public static int[][][] createArray() {
-    int[][][] arrayTemp = new int[5][4][3];
+  public static int[][] create2dArray() {
+    int[][] arrayTemp = new int[randomFill()][randomFill()];
+    return arrayTemp;
+  }
+
+  public static int[][][] create3dArray() {
+    int[][][] arrayTemp = new int[randomFill()][randomFill()][randomFill()];
     return arrayTemp;
   }
 
@@ -51,7 +62,7 @@ public class ArrayStuff {
 
   public static int randomFill() {
     Random random = new Random();
-    int randomNumber = random.nextInt(255);
+    int randomNumber = random.nextInt(3);
     return randomNumber;
   }
 
@@ -61,52 +72,28 @@ public class ArrayStuff {
   *  based on that.
   */
 
-  public static int[][][] fillArray(int[][][] inputArray) {
+  public static int[][] fillArray(int[][] inputArray) {
     for (int i = 0; i < inputArray.length; i++) {
       for (int j = 0; j < inputArray[i].length; j++) {
-        try {
-          for (int k = 0; k < inputArray[j].length; k++) {
-            inputArray[i][j][k] = randomFill();
-          }
-        } catch (ArrayIndexOutOfBoundsException e) {
-          // Do nothing because I'm a piece of shit and so is this workaround
-        }
+          inputArray[i][j] = randomFill();
       }
     }
     return inputArray;
   }
 
-  public static int[][][] meanArray(int[][][] inputArray1, int[][][] inputArray2) {
-    
-  }
+  // public static int[][][] meanArray(int[][][] inputArray1, int[][][] inputArray2) {
+  //
+  // }
 
   // public static int[][][] medianArray(int[][][] inputArray) {
   //
   // }
 
-  public static void printArray(int[][][] inputArray) {
-    for (int i = 0; i < inputArray.length; i++) {
-      System.out.print("[");
-      for (int j = 0; j < inputArray[i].length; j++) {
-        System.out.print("{");
-        try {
-          for (int k = 0; k < inputArray[j].length; k++) {
-            if (k < (inputArray[j].length - 1)) {
-              System.out.print(" " + inputArray[i][j][k] + " ");
-            } else {
-              System.out.print(inputArray[i][j][k]);
-            }
-          }
-        } catch (ArrayIndexOutOfBoundsException e) {
-          // Do nothing because I'm a piece of shit and so is this workaround
-        }
-        if (j < (inputArray[i].length - 1)) {
-          System.out.print("}, ");
-        } else {
-          System.out.print("}");
-        }
-      }
-      System.out.print("]\n");
-    }
+  public static void print2dArray(int[][] inputArray) {
+    System.out.println(Arrays.deepToString(inputArray));
+  }
+
+  public static void print3dArray(int[][][] inputArray) {
+    System.out.println(Arrays.deepToString(inputArray));
   }
 }
